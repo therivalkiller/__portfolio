4d663fd9425a53beea734747ef01c604d03e0167
therivalkiller <97250364+therivalkiller@users.noreply.github.com>
2022-07-01 02:26:21 +0530

    Update projects.js

--- src/resources/projects.js
+++ src/resources/projects.js
@@ -11,6 +11,12 @@ const projectsData = [
     description: "PlantKart is built using MERN Stack. PlantKart in an online e-commerce platform for connecting customers to the local nurseries around them and get plants delivered to their homes directly from these Nurseries.",
     link: "https://plantkartapp.herokuapp.com/",
   },
+  {
+    title: "Weather App",
+    image: "./weather.jpg",
+    description: "Built using pure HTML, CSS and JavaScript",
+    link: "https://weather-detective.netlify.app/",
+  }
 ];
 
 export default projectsData;
